896ac86354d783a340f6d2f714b19ebebb92f59d
Ppgraca <58826773+Ppgraca@users.noreply.github.com>
2024-03-22 13:44:28 +0000

    ist1110866_Proj2.py

--- ist1110866_Proj2.py
+++ ist1110866_Proj2.py
@@ -96,6 +96,7 @@ fig2 = px.line(df_results,
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+server = app.server
 
 app.layout = html.Div([
     html.H1('ist1110866 Forecast tool - Energy Services'),
